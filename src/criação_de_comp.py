6a3e006a11f39751971cb4369f4c08e0671d8196
Sergio Luiz Semprebom <sergiosemprebom@gmail.com>
2025-03-15 16:28:22 -0300

    update-Estru_felt_page

--- src/criação_de_comp.py
+++ src/criação_de_comp.py
@@ -5,7 +5,7 @@ def main(page: ft.Page):
     t1 = ft.Text(
         value='Olá seja bem vindo ao GED, Gestão Eletonica de Documentação', # Criando um texto
         style = ft.TextThemeStyle.DISPLAY_LARGE, # Formatando o estilo do Texto
-        bgcolor = ft.colors.AMBER # Fundo das letras
+        bgcolor = ft.Colors.AMBER # Fundo das letras
     )
     page.add(t1) # Obrigatorio para um texto
 ft.app(target=main) # Obirgatorio
---
+++ src/page_1.py
@@ -0,0 +1,34 @@
+
+import flet as ft
+
+def main(page: ft.Page):
+
+    # aletrando fundo da pagina
+    page.bgcolor = 'green' # alterando a cor d fundo d pagina com um variavel
+    page.bgcolor = '#B12B12' # alterando a cor d fundo d pagina com um variavel
+    page.bgcolor = ft.Colors.WHITE # alterando a cor d fundo padrao
+    
+    #Formas de alinhamento na horizontal o de texto
+    page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH # alinhamento horizontal    
+    #page.horizontal_alignmet = ft.CrossAxisAlignment.CENTER # alinhamento central
+    
+    #Formas de alinhamento na vertical o de texto
+    page.vertical_alignment = ft.MainAxisAlignment.START  # alinhamento vertical
+
+    # Formas de Espaçamento
+    #page.padding = ft.padding.all(30) # margem de 10px
+    #page.padding = ft.padding.symmetric(vertical=100, horizontal=10)
+    #page.padding = ft.padding.only(top=20, left=10, right=100, bottom=50) # será usado no curso
+    page.padding = 20
+    page.spacing = 100
+
+
+    # alinhamento
+    page.add(
+        ft.Text(value='Sou Programador FullStack'),
+        ft.Container(ft.Text(value='Sou programador FullStack'), bgcolor='AMBER')
+    )
+    
+    page.update() # parametro update, fica vazio mas renderiza a pagina de forma automatica
+
+ft.app(target = main)
